0ed455dfc56110e5cdcfe6a00d9976136f061f4c
Oleksandra Voichenko <ol.voichenko@gmail.com>
2024-10-02 22:42:44 +0300

    make menu buttons functional

--- code/ui.py
+++ code/ui.py
@@ -1,3 +1,5 @@
+import pygame
+
 from settings import *
 
 class UI:
@@ -7,6 +9,16 @@ class UI:
         self.left = WINDOW_WIDTH / 2 - 100
         self.top = WINDOW_HEIGHT / 2 + 50
         self.monster = monster
+
+        # controls
+        self.general_options = ['attack', 'heal', 'switch', 'escape']
+        self.general_idx = {'col' : 0, 'row' : 0}
+
+
+    def input(self):
+        keys = pygame.key.get_just_pressed()
+        self.general_idx['row'] += int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+        self.general_idx['col'] += int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
 
 
     def general(self):
@@ -21,9 +33,16 @@ class UI:
             for row in range(rows):
                 x = rect.left + rect.width / 4 + (rect.width / 2) * col
                 y = rect.top + rect.height / 4 + (rect.height / 2) * row
-                text_surf = self.font.render('option', True, 'black')
+
+                i = col + 2 * row
+
+                text_surf = self.font.render(self.general_options[i], True, 'black')
                 text_rect = text_surf.get_frect(center = (x, y))
                 self.display_surface.blit(text_surf, text_rect)
+
+
+    def update(self):
+        self.input()
 
 
     def draw(self):
